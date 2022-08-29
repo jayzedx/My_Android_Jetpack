b44470fba20c2e739627fe757bf24f10deeab48c
jayzedx <d.chalermpong@gmail.com>
2022-08-30 00:05:25 +0700

    Chapter1 - Adding scrolling to the colume for listview

--- Chapter1_Compose/app/src/main/java/com/tutorial/chapter1/myapplication/Model/Restaurant.kt
+++ Chapter1_Compose/app/src/main/java/com/tutorial/chapter1/myapplication/Model/Restaurant.kt
@@ -5,5 +5,19 @@ data class Restaurant(val id: Int, val title: String, val description: String)
 val dummyRestaurants = listOf(
     Restaurant(0, "Alfredo foods", "At Alfredo's …"),
     Restaurant(1, "Jay or Jim Shopping", ""),
-    Restaurant(2, "Mike and Ben's food pub", "")
-)
+    Restaurant(2, "Mike and Ben's food pub", ""),
+    Restaurant(3, "Alfredo foods", "At Alfredo's …"),
+    Restaurant(4, "Jay or Jim Shopping", ""),
+    Restaurant(5, "Mike and Ben's food pub", ""),
+    Restaurant(6, "Alfredo foods", "At Alfredo's …"),
+    Restaurant(7, "Jay or Jim Shopping", ""),
+    Restaurant(8, "Mike and Ben's food pub", ""),
+    Restaurant(9, "Jay or Jim Shopping", ""),
+    Restaurant(10, "Mike and Ben's food pub", ""),
+    Restaurant(11, "Alfredo foods", "At Alfredo's …"),
+    Restaurant(12, "Jay or Jim Shopping", ""),
+    Restaurant(13, "Mike and Ben's food pub", ""),
+    Restaurant(14, "Alfredo foods", "At Alfredo's …"),
+    Restaurant(15, "Jay or Jim Shopping", ""),
+    Restaurant(16, "Mike and Ben's food pub", "")
+    )
--- Chapter1_Compose/app/src/main/java/com/tutorial/chapter1/myapplication/Screen/RestaurantScreen.kt
+++ Chapter1_Compose/app/src/main/java/com/tutorial/chapter1/myapplication/Screen/RestaurantScreen.kt
@@ -1,7 +1,10 @@
 package com.tutorial.chapter1.myapplication.Screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tutorial.chapter1.myapplication.Component.RestaurantItem
 import com.tutorial.chapter1.myapplication.Model.dummyRestaurants
@@ -9,7 +12,7 @@ import com.tutorial.chapter1.myapplication.ui.theme.MyApplicationTheme
 
 @Composable
 fun RestaurantScreen() {
-    Column {
+    Column(Modifier.verticalScroll(rememberScrollState())) {
         dummyRestaurants.forEach { item ->
             RestaurantItem(item)
         }
