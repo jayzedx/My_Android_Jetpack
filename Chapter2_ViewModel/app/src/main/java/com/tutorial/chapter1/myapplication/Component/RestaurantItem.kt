b2f0601c34ceb17b98da5b4ace2771f3fcba2386
jayzedx <d.chalermpong@gmail.com>
2022-08-30 01:15:58 +0700

    Chapter2 - Refactoring Hoisting state in compose (move logic to parent)

--- Chapter2_ViewModel/app/src/main/java/com/tutorial/chapter1/myapplication/Component/RestaurantItem.kt
+++ Chapter2_ViewModel/app/src/main/java/com/tutorial/chapter1/myapplication/Component/RestaurantItem.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -13,7 +17,14 @@ import com.tutorial.chapter1.myapplication.Model.Restaurant
 
 
 @Composable
-fun RestaurantItem(item: Restaurant) {
+fun RestaurantItem(item: Restaurant, onClick: (id: Int) -> Unit) {
+
+    val icon = if (item.isFavorite)
+        Icons.Filled.Favorite
+    else
+        Icons.Filled.FavoriteBorder
+
+
     Card(
         elevation = 8.dp,
         modifier = Modifier.padding(8.dp)
@@ -27,7 +38,11 @@ fun RestaurantItem(item: Restaurant) {
                 item.title, item.description,
                 Modifier.weight(0.85f)
             )
-            FavoriteIcon(Modifier.weight(0.15f))
+
+            //FavoriteIcon(Modifier.weight(0.15f))
+            RestaurantIcon(icon, Modifier.weight(0.15f)) {
+                onClick(item.id)
+            }
         }
     }
 }
