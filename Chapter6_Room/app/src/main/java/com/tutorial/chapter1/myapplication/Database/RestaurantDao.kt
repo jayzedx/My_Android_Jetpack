4fd3a9f16caecbcac0b7730732e284faf876a5fc
jayzedx <d.chalermpong@gmail.com>
2022-08-30 19:07:23 +0700

    Chapter6 - Fixing missing annotation dao

--- Chapter6_Room/app/src/main/java/com/tutorial/chapter1/myapplication/Database/RestaurantDao.kt
+++ Chapter6_Room/app/src/main/java/com/tutorial/chapter1/myapplication/Database/RestaurantDao.kt
@@ -1,10 +1,12 @@
 package com.tutorial.chapter1.myapplication.Database
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.tutorial.chapter1.myapplication.Model.Restaurant
 
+@Dao
 interface RestaurantDao {
     @Query("SELECT * FROM restaurants")
     suspend fun getAll(): List<Restaurant>
