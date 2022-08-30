90549992d1f8715d75754d185c01c52182c11cad
jayzedx <d.chalermpong@gmail.com>
2022-08-30 14:57:29 +0700

    Chapter3 - Adding more rest

--- Chapter3_Retrofit/app/src/main/java/com/tutorial/chapter1/myapplication/Network/RestaurantsApiService.kt
+++ Chapter3_Retrofit/app/src/main/java/com/tutorial/chapter1/myapplication/Network/RestaurantsApiService.kt
@@ -2,9 +2,14 @@ package com.tutorial.chapter1.myapplication.Network
 
 import com.tutorial.chapter1.myapplication.Model.Restaurant
 import retrofit2.Call
+import retrofit2.http.Field
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface RestaurantsApiService {
     @GET("restaurants.json")
     fun getRestaurants(): Call<List<Restaurant>>
+
+//    @POST("user/edit")
+//    fun updateUser(@Field("first_name") firstName: String): Call<User>
 }
